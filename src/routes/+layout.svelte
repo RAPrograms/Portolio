29d74d5a3171b130e7f5bcad31c7530422892945
Dog2657 <RAPrograms@outlook.com>
2025-12-08 22:26:54 +0000

    Added fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
-	import "../main.scss";
+	import "../styling/main.scss";
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<link rel="preload" href="/fonts/spacegrotesk/spacegrotesk.woff2" as="font" type="font/woff2">
+	<link rel="preload" href="/fonts/spacegrotesk/spacegrotesk.woff" as="font" type="font/woff2">
+</svelte:head>
 
 {@render children()}
