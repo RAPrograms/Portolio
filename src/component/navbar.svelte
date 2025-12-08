4a7363978baee68d0c696ecd50ed24320072a04e
Dog2657 <RAPrograms@outlook.com>
2025-12-08 23:08:03 +0000

    Styled navigation bar

--- src/component/navbar.svelte
+++ src/component/navbar.svelte
@@ -1,13 +1,59 @@
 <header>
     <nav>
-        Navigation
+        <a href="/">
+            <img src="/logos/primary.svg" alt="RAPrograms Logo">
+        </a>
+
+        <ul class="links">
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#technology">Technology</a>
+            </li>
+            <li>
+                <a href="#projects">Projects</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
     </nav>
 </header>
 
 
 <style lang="scss">
+    @use "$styling/_variables.scss" as *;
+
     header{
+        border-bottom: 1px solid var(--border-colour);
         height: var(--nav-height);
+        position: sticky;
+        top: 0;
+        left: 0;
+        backdrop-filter: blur(24px);
         display: block;
+
+        & > nav{
+            @include center_content();
+
+            max-width: var(--max-content-width);
+            justify-content: space-between;
+            align-items: center;
+            display: flex;
+            height: 100%;
+
+
+            & > ul.links{
+                list-style-type: none;
+                display: flex;
+                gap: 10px;
+
+                a{
+                    color: var(--secondary-text-colour);
+                    text-decoration: none;
+                }
+            }
+        }
     }
 </style>
