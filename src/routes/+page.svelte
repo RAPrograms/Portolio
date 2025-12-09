6b0ad9c58db5077fc8cb1b9abf20843e81a8df0f
RAPrograms <RAPrograms@outlook.com>
2025-12-09 07:27:39 +0000

    Adding arguments to hero component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,9 @@
 
 </script>
 
-<Hero/>
+<Hero flavor_text="Sleep && Eat && Game && Code">
+    Passionate about creating digital software with expertise in <span>web development</span>, <span>back-end systems</span>, and <span>application development</span>.
+</Hero>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
