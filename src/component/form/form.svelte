7b2e66ff9ef7ad0eca1b6eba4b0bcf89a78dd9bf
Dog2657 <RAPrograms@outlook.com>
2025-12-12 12:12:58 +0000

    Added contact form styling and structure

---
+++ src/component/form/form.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+    const { children } : { children: Snippet } = $props()
+
+    function submit(e){
+        e.preventDefault()
+
+        console.log(e.target)
+    }
+</script>
+
+<form onsubmit={submit}>
+    {@render children()}
+
+    <button type="submit">Submit</button>
+</form>
+
+<style lang="scss">
+    @use "$styling/_variables.scss" as *;
+
+    form{
+        @include glass-card();
+
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+
+        & > button[type=submit]{
+            @include coloured-tag(rgb(0, 200, 0));
+
+            margin-top: 10px;
+        }
+    }
+</style>
