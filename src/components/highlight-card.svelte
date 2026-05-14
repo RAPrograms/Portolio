62e3aff8e80a4bc9ad9c17f18a127e46c15d941b
RAPrograms <RAPrograms@outlook.com>
2026-05-14 13:47:53 +0100

    Moving heighlight card styling to extendable class

--- src/components/highlight-card.svelte
+++ src/components/highlight-card.svelte
@@ -31,28 +31,10 @@
 </article>
 
 <style lang="scss">
-    article {
-        background: linear-gradient(-210deg, color-mix(in hsl shorter hue, var(--primary-colour) 60%, transparent), #242931 30%);
-        flex-direction: column;
-        border-radius: 12px;
-        position: relative;
-        isolation: isolate;
-        text-align: left;
-        display: flex;
-        padding: 30px;
-        gap: 15px;
+    @use "$styling/_variables.scss" as *;
 
-        &::after{
-            background-color: #242931;
-            height: calc(100% - 4px);
-            width: calc(100% - 4px);
-            border-radius: inherit;
-            position: absolute;
-            z-index: -1;
-            content: "";
-            left: 2px;
-            top: 2px;
-        }
+    article {
+        @extend %acent-card;
 
         & >  div{
             background-color: color-mix(in hsl shorter hue, var(--primary-colour) 20%, transparent);
