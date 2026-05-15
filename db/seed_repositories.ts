dfa334d882017a56bbf5ab8675e1f0f963590885
RAPrograms <RAPrograms@outlook.com>
2026-05-15 21:42:40 +0100

    Impliment repo filtering for empty descriptions

--- db/seed_repositories.ts
+++ db/seed_repositories.ts
@@ -14,14 +14,13 @@ const insertRepos = db.transaction(repos => {
     for (const repo of repos){
         // Hide repos that are not finished
         // Indicated by a lack of a description
-        //if(repo["description"] == "")
-        //    continue
+        if(repo["description"] == "")
+            continue
 
         // Prevent processing github main repo
         if(import.meta.env.GITHUB_USERNAME.toLowerCase() == repo["name"].toLowerCase())
             continue
         
-
         const record: Record<string, any> = {
             "title": repo["name"],
             "description": repo["description"],
