806fbbebad050e4a4db5913b0aeba6890a426d60
RAPrograms <RAPrograms@outlook.com>
2026-05-15 22:19:40 +0100

    Loading featured projects from action vars

--- db/seed_repositories.ts
+++ db/seed_repositories.ts
@@ -2,63 +2,95 @@ import { Frameworks, Langauages, ProjectTypes, Stack, Technologies, Tools } from
 
 import { readFile } from 'node:fs/promises';
 import { Database } from "bun:sqlite";
+import { TOML } from "bun";
 
 const db = new Database('./db/data.db', { strict: true });
 
-const insert = db.prepare(
-    `INSERT INTO projects (title, description, type, image_uri, repository_url, demo_url, tags)
-    VALUES (:title, :description, :type, :image_uri, :repository_url, :demo_url, :tags)`
-);
+async function seedProjects(){
+    const insert = db.prepare(
+        `INSERT INTO projects (title, description, type, image_uri, repository_url, demo_url, tags)
+        VALUES (:title, :description, :type, :image_uri, :repository_url, :demo_url, :tags)`
+    );
 
-const insertRepos = db.transaction(repos => {
-    for (const repo of repos){
-        // Hide repos that are not finished
-        // Indicated by a lack of a description
-        if(repo["description"] == "")
-            continue
-
-        // Prevent processing github main repo
-        if(import.meta.env.GITHUB_USERNAME.toLowerCase() == repo["name"].toLowerCase())
-            continue
-        
-        const record: Record<string, any> = {
-            "title": repo["name"],
-            "description": repo["description"],
-            "repository_url": (repo["url"] != "")? repo["url"]:null,
-            "demo_url": repo["homepageUrl"] ?? null,
-            "image_uri": null //TODO: add images
-        }
-
-        const projectType: Array<typeof ProjectTypes> = []
-        const projectTags: Array<Stack> = []
-
-        for(const {name: tag} of repo["repositoryTopics"] || []){
-            // Check for project type tags
-            // @ts-ignore
-            if(ProjectTypes[tag]){
-                projectType.push(tag)
+    const insertRepos = db.transaction(repos => {
+        for (const repo of repos){
+            // Hide repos that are not finished
+            // Indicated by a lack of a description
+            if(repo["description"] == "")
                 continue
+
+            // Prevent processing github main repo
+            if(import.meta.env.GITHUB_USERNAME.toLowerCase() == repo["name"].toLowerCase())
+                continue
+            
+            const record: Record<string, any> = {
+                "title": repo["name"],
+                "description": repo["description"],
+                "repository_url": (repo["url"] != "")? repo["url"]:null,
+                "demo_url": repo["homepageUrl"] ?? null,
+                "image_uri": null //TODO: add images
             }
 
-            // Check if the tag is supported
-            for(const group of [Langauages, Technologies, Frameworks, Tools]){
+            const projectType: Array<typeof ProjectTypes> = []
+            const projectTags: Array<Stack> = []
+
+            for(const {name: tag} of repo["repositoryTopics"] || []){
+                // Check for project type tags
                 // @ts-ignore
-                if(group[tag]){
-                    projectTags.push(tag)
-                    break
+                if(ProjectTypes[tag]){
+                    projectType.push(tag)
+                    continue
+                }
+
+                // Check if the tag is supported
+                for(const group of [Langauages, Technologies, Frameworks, Tools]){
+                    // @ts-ignore
+                    if(group[tag]){
+                        projectTags.push(tag)
+                        break
+                    }
                 }
             }
+
+            record["type"] = JSON.stringify(projectType)
+            record["tags"] = JSON.stringify(projectTags)
+            
+            insert.run(record);
         }
 
-        record["type"] = JSON.stringify(projectType)
-        record["tags"] = JSON.stringify(projectTags)
-        
-        insert.run(record);
-    }
+        return repos.length
+    });
 
-    return repos.length
-});
+    const json = await readFile('./db/repos.json', 'utf8').then((raw: string) => JSON.parse(raw));
 
-const json = await readFile('./db/repos.json', 'utf8').then((raw: string) => JSON.parse(raw));
+    insertRepos(json);
+}
 
-insertRepos(json);
+function seedFeaturedProjects(){
+    const raw = import.meta?.env?.FEATURED_PROJECTS || ""
+    const data = TOML.parse(raw);
+
+    const insert = db.prepare(
+        `INSERT INTO projects (project_id, caption, theme)
+        VALUES (:project_id, :caption, :theme)`
+    );
+
+    const insertRecords = db.transaction(([repo_name, details]) => {
+        const id = db.query(
+            `SELECT id FROM projects
+            WHERE title = ?
+            LIMIT 1`
+        ).get("Rust-Wordle-Api")["id"]
+
+        insert.run({
+            project_id: id,
+            caption: details["caption"],
+            theme: details["theme"]
+        })
+    })
+
+    insertRecords(Object.entries(data))
+}
+
+await seedProjects()
+seedFeaturedProjects()
