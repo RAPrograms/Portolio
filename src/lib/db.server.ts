a6eeaa0994d783c024852320ac32fc8a4cf1d1eb
RAPrograms <RAPrograms@outlook.com>
2026-05-24 21:49:45 +0100

    Removed sqlite database loading and seeding code

--- src/lib/db.server.ts
+++
@@ -1,9 +0,0 @@
-import Database from 'better-sqlite3';
-import { resolve } from 'path';
-
-const path = resolve('data/data.db')
-
-export default new Database(path, {
-    verbose: (import.meta.env.DEV)? console.log:null,
-    readonly: true
-});
