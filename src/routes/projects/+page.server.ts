0c7c7a803426db1716778e85632b19564e4bb9ba
RAPrograms <RAPrograms@outlook.com>
2026-05-24 22:54:53 +0100

    Corrected project filtering

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -7,6 +7,12 @@ export const prerender = true;
 // This will bake the project records into the page
 export const load: PageServerLoad = async ({ params }) => {
 	return {
-		records: Object.values(projects)
+		records: Object.values(projects).map(project => {
+			if(typeof project["type"] == "string")
+				// @ts-ignore
+				project["type"] = [project["type"]]
+
+			return project
+		})
 	};
 };
